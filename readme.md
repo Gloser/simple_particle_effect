4b0b1fa896e638eaf0cb6fc0489c5764b1d0d2d9
Rene Jonas <rene.jonas.rc@googlemail.com>
2019-05-02 08:16:41 +0200

    add comments and complete readme.md

---
+++ readme.md
@@ -0,0 +1,26 @@
+# simple particle effect # 
+
+
+### eng ###
+Simple particle effect written in rust lang. Inspired by Coding Challenge #78 by The Coding Train
+
+
+### de ###
+Ein einfacher Partikel-Effekt geschrieben in Rust. Als Vorlage/Inspiration diente die Coding Challenge Nr. 78 von The Coding Train.
+
+
+### additional information ###
+Coding Challenge: https://youtu.be/UcdigVaIYAk
+rust-lang : https://www.rust-lang.org/
+raylib: https://www.raylib.com/
+rust-bindings: https://github.com/AregevDev/raylib-rs
+
+
+## CREDITS ##
+Many Thanks to raysan5 (https://github.com/raysan5/raylib) and of course
+AregevDev (https://github.com/AregevDev/raylib-rs) and deltaphc (https://github.com/deltaphc/raylib-rs)
+for the rust-bindings
+
+
+### screenshot ###
+![Alt text](/screenshot.png?raw=true "simple particle effect in rust lang - Screenshot")
--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,77 @@
-
-
 use raylib::prelude::*;
 use rand::Rng;
 
 struct Particle<'a> {
     
-        x: f32,
-        y: f32,
-        vx: f32,
-        vy: f32,
-        alpha: u8,
-        rl: &'a raylib::RaylibHandle
+    x: f32,
+    y: f32,
+    vx: f32,
+    vy: f32,
+    alpha: u8,
+    rl: &'a raylib::RaylibHandle
 }
 
 impl<'a> Particle<'a> {
     
-        pub fn show(&self) {
+    pub fn show(&self) {
         
-            self.rl.draw_circle_v(vec2(self.x, self.y), 10., Color::new(200, 200, 200, self.alpha));
+        self.rl.draw_circle_v(vec2(self.x, self.y), 10., Color::new(200, 200, 200, self.alpha));
     }
-    
-    
+        
     pub fn update(&mut self) {
         
-            self.x += self.vx;
-            self.y += self.vy;
+        self.x += self.vx;
+        self.y += self.vy;
         
-            if self.alpha > 0 {
+        if self.alpha > 0 {
             
-                self.alpha -= 1;
+            self.alpha -= 1;
         }
     }
 }
 
 
 fn main() {
-        let rl = raylib::init().size(800, 600).title("Simple Particle").msaa_4x().build();
-        let mut rng = rand::thread_rng();
+
+    // initialize raylib
+    let rl = raylib::init().size(800, 600).title("Simple Particle").msaa_4x().build();
+    let mut rng = rand::thread_rng();
+
+    // vector of particles
+    let mut particles: Vec<Particle> = Vec::new();
+
+    // limit frames per second to 60
+    rl.set_target_fps(60);
+
+    while !rl.window_should_close() {
     
-        let mut particles: Vec<Particle> = Vec::new();
-    
-        rl.set_target_fps(60);
-    
-        while !rl.window_should_close() {
-        
-            for _ in 0..5 {
-            
-                particles.push(Particle{
-                    x: (rl.get_screen_width() / 2) as f32,
-                    y: (rl.get_screen_height()) as f32,
-                    vx: rng.gen_range(-1., 1.),
-                    vy: rng.gen_range(-5., -1.),
-                    alpha: 255,
-                    rl: &rl
+        for _ in 0..5 {
+
+            // add particles to vector
+            particles.push(Particle{
+                x: (rl.get_screen_width() / 2) as f32,
+                y: (rl.get_screen_height()) as f32,
+                vx: rng.gen_range(-1., 1.),
+                vy: rng.gen_range(-5., -1.),
+                alpha: 255,
+                rl: &rl
             });
         }
-        
-        rl.begin_drawing();
-            rl.clear_background(Color::new(20, 20, 20, 255));
-        
-            for p in &mut particles {
-                p.update();
-                p.show();
-        }
-        
-        particles.retain(|x| x.alpha > 1);
-        
-            rl.draw_text(&particles.len().to_string(), 10, 10, 18, Color::new(200, 200, 200, 255));
-        
-            rl.end_drawing();
-    }
     
+        rl.begin_drawing();
+        rl.clear_background(Color::new(20, 20, 20, 255));
+    
+        // update an show all particles
+        for p in &mut particles {
+            p.update();
+            p.show();
+        }
+    
+        // retain particles as long as alpha value is greater than one
+        particles.retain(|x| x.alpha > 1);
+    
+        rl.draw_text(&particles.len().to_string(), 10, 10, 18, Color::new(200, 200, 200, 255));
+
+        rl.end_drawing();
+    }
 }
